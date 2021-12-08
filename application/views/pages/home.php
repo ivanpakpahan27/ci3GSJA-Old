10db90b7ff806601a18fefa93e784c5cae5fff1b
Ivan Pakpahan <ivanpakpahangn@gmail.com>
2021-12-08 17:38:50 +0700

    3

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,33 +1,35 @@
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
+<div class="carousel-home">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-        <div class="item active">
-            <img src="<?php echo base_url(); ?>assets/images/home/1.jpg" alt="bagian-depan" style="width:100%;">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="<?php echo base_url(); ?>assets/images/home/1.jpg" alt="bagian-depan" style="width:100%;">
+            </div>
+
+            <div class="item">
+                <img src="<?php echo base_url(); ?>assets/images/home/2.jpg" alt="bagian-depan 2" style="width:100%;">
+            </div>
+
+            <div class="item">
+                <img src="<?php echo base_url(); ?>assets/images/home/3.jpg" alt="bagian-plank" style="width:100%;">
+            </div>
         </div>
 
-        <div class="item">
-            <img src="<?php echo base_url(); ?>assets/images/home/2.jpg" alt="bagian-depan 2" style="width:100%;">
-        </div>
-
-        <div class="item">
-            <img src="<?php echo base_url(); ?>assets/images/home/3.jpg" alt="bagian-plank" style="width:100%;">
-        </div>
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
-    </a>
 </div>
