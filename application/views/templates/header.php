fb4b0923c111297bfa1fb8de459a92843d5caaf3
Ivan Pakpahan <ivanpakpahangn@gmail.com>
2021-12-08 16:23:53 +0700

    first commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+    <title>GSJA Pemuridan</title>
+    <!-- <link rel="stylesheet" href="https://bootswatch.com/5/flatly/bootstrap.min.css"> -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
+
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="logo-web" href="#"><img src="https://i0.wp.com/www.gsjabatutulis.com/wp-content/uploads/2017/03/Logo-GSJA-bolong.png?fit=240%2C227&ssl=1" alt="Logo"></a>
+                <a class="navbar-brand" href="#">Pemuridan</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="<?php echo base_url(); ?>">Beranda</a></li>
+                <li><a href="<?php echo base_url(); ?>about">Tentang</a></li>
+                <li><a href="<?php echo base_url(); ?>posts">Postingan</a></li>
+                <li><a href="<?php echo base_url(); ?>activity">Kegiatan</a></li>
+                <li><a href="<?php echo base_url(); ?>gallery">Galeri</a></li>
+                <li><a href="<?php echo base_url(); ?>categories">Kategori</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
